a0178190ef70283848a1f82a179b28cb5ca7e84e
KingISkin <joaovictornunes076@gmail.com>
2023-11-05 00:02:31 -0300

    update banco de dados

--- JogoAdivinhacao.py
+++ JogoAdivinhacao.py
@@ -1,54 +1,44 @@
-#-*- coding: utf-8 -*-
-
-#Imports
 import tkinter as tk
 from tkinter import messagebox
 import random
 import sqlite3
-import os
 from datetime import datetime
 
-#sqlite3 para poder interagir com o banco de dados sqlite
-#os para realizar a execução de comandos do S.O ('clear' para Linux e Mac, 'cls' para windows)
-#datetime para trabalhar com datas e horas
+#importações: o codigo começa importando os módulos necessários para o jogo:
+#tkinter interface grafica do python
+#sqlite3 para interagir com um banco de dados SQLite.
+#datetime para trabalhar com datas e horas.
 
-def criar_tabela_partidas():
-    try:
-        conn = sqlite3.connect("bancodedados.db")
-        cursor = conn.cursor()
+def criar_tabela_partidas(): #função criar_tabela_partidas responsável por criar uma tabela chamada "partidas" no banco de dados SQLite 'exemplo.db', se ela ainda não existir.
+    conn = sqlite3.connect("bancodedados.db")
+    cursor = conn.cursor() #é estabelecida uma conexão com o banco de dados SQLite 'bancodedados.db' usando a função 'connect' do módulo 'sqlite3'. Se o banco de dados não existir, ele será criado neste momento.
 
-        cursor.execute(
-            """
-            CREATE TABLE IF NOT EXISTS partidas (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                nome_usuario TEXT,
-                tentativas INTEGER,
-                inicio DATETIME,
-                fim DATETIME)""")
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS partidas (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome_usuario TEXT,
+            tentativas INTEGER,
+            inicio DATETIME,
+            fim DATETIME)""")
+    # ^aqui foi feita uma instrução sql para criar uma tabela chamada 'partidas' caso ela nao exista. e a tabela tem as colunas: id, nome de usuario, tentativas, inicio e fim.
+    conn.commit()
+    conn.close()#dps de criar a tabela no banco de dados, eu fecho a conexao com o banco de dados
 
-        conn.close()  #dps de criar a tabela no banco de dados, eu fecho a conexao com o banco de dados
-    except sqlite3.Error as e:
-        print("Erro ao criar o banco de dados:", e)
+def inserir_partida(nome_usuario, tentativas, inicio, fim):
+    conn = sqlite3.connect("bancodedados.db")
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO partidas (nome_usuario, tentativas, inicio, fim) VALUES (?, ?, ?, ?)",
+        (nome_usuario, tentativas, inicio.strftime('%d-%m-%Y %H:%M:%S'), fim.strftime('%d-%m-%Y %H:%M:%S')))   
+    conn.commit()
+    conn.close()  # dps de inserir os dados, a conexão com o banco de dados é fechada.
 
 def iniciar_jogo():
-    global nickname
+    global nickname, inicio_jogo
     nickname = entrada_nickname.get()
     if not nickname:
         messagebox.showerror("Nickname Inválido", "Digite um nickname válido.")
     else:
-        criar_tabela_partidas()  # Chame a função para criar a tabela no início do jogo
-        frame_inicio.pack_forget()
-        frame_jogo.pack()
-        reiniciar_jogo()
-
-# ^aqui foi feita uma instrução sql para criar uma tabela chamada 'partidas' caso ela nao exista. e a tabela tem as colunas: id, nome de usuario, tentativas, inicio e fim.
-
-def iniciar_jogo():
-    global nickname
-    nickname = entrada_nickname.get()
-    if not nickname:
-        messagebox.showerror("Nickname Inválido", "Digite um nickname válido.")
-    else:
+        criar_tabela_partidas() 
         frame_inicio.pack_forget()
         frame_jogo.pack()
         reiniciar_jogo()
@@ -70,6 +60,8 @@ def comparar_palpite(palpite):
     
     if palpite == numero_secreto:
         messagebox.showinfo("Parabéns!", f"{nickname}, você acertou em {tentativas} tentativas.")
+        inicio_jogo = datetime.now() 
+        inserir_partida(nickname, tentativas, inicio_jogo, datetime.now())
         reiniciar_jogo()
     elif palpite < numero_secreto:
         messagebox.showinfo("Palpite Baixo", "Tente um número maior.")
@@ -77,15 +69,17 @@ def comparar_palpite(palpite):
         messagebox.showinfo("Palpite Alto", "Tente um número menor.")
         
 def reiniciar_jogo():
-    global numero_secreto, tentativas
-    numero_secreto = random.randint(0, 100)
+    global numero_secreto, tentativas, inicio_jogo
+    numero_secreto = random.randint(1, 100) #a funcao inicia definindo um numero aleatorio entre 1 e 100 e inicializa o contador de tentativas como 0.
     tentativas = 0
     entrada.delete(0, tk.END)
     tentativas_label.config(text=f"Tentativas: {tentativas}")
+    inicio_jogo = datetime.now()  #obtida a data e hora atuais
 
-numero_secreto = random.randint(0, 100)
+numero_secreto = random.randint(1, 100)
 tentativas = 0
 nickname = ""
+inicio_jogo = datetime.now()
 
 #interface grafica
 root = tk.Tk()
